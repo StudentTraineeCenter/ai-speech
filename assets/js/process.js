68e62794394f24ed6f1d8eba28005909061522ef
Filip Troníček <filip.tronicek@seznam.cz>
2020-10-12 16:15:11 +0200

    Updates and static serving

--- assets/js/process.js
+++ assets/js/process.js
@@ -1,66 +1,74 @@
-let phraseDiv;
-let startRecognizeOnceAsyncButton;
+/* DOM */
+const startRecognizeOnceAsyncButton = document.getElementById(
+  "startRecognizeOnceAsyncButton"
+);
+const subscriptionKey = document.getElementById("subscriptionKey");
+const serviceRegion = document.getElementById("serviceRegion");
+const phraseDiv = document.getElementById("phraseDiv");
 
-let subscriptionKey, serviceRegion;
 let authorizationToken;
 let SpeechSDK;
 let recognizer;
 
-document.addEventListener("DOMContentLoaded", function () {
-  startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
-  subscriptionKey = document.getElementById("subscriptionKey");
-  serviceRegion = document.getElementById("serviceRegion");
-  phraseDiv = document.getElementById("phraseDiv");
-
-  startRecognizeOnceAsyncButton.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", () => {
+  startRecognizeOnceAsyncButton.addEventListener("click", () => {
     startRecognizeOnceAsyncButton.disabled = true;
     phraseDiv.innerHTML = "";
 
     // if we got an authorization token, use the token. Otherwise use the provided subscription key
     let speechConfig;
     if (authorizationToken) {
-      speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(authorizationToken, serviceRegion.value);
+      speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(
+        authorizationToken,
+        serviceRegion.value
+      );
     } else {
-      if (subscriptionKey.value === "" || subscriptionKey.value === "subscription") {
+      if (
+        subscriptionKey.value === "" ||
+        subscriptionKey.value === "subscription"
+      ) {
         alert("Zadejte klíč!");
+        startRecognizeOnceAsyncButton.disabled = false;
         return;
       }
-      speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey.value, serviceRegion.value);
+      speechConfig = SpeechSDK.SpeechConfig.fromSubscription(
+        subscriptionKey.value,
+        serviceRegion.value
+      );
     }
 
     speechConfig.speechRecognitionLanguage = "en-US";
-    const audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    const audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
     recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
 
     recognizer.recognizeOnceAsync(
-      function (result) {
+      (result) => {
         startRecognizeOnceAsyncButton.disabled = false;
-        console.log(result.privText);
         phraseDiv.value = result.privText;
 
         recognizer.close();
         recognizer = undefined;
       },
-      function (err) {
+      (err) => {
         startRecognizeOnceAsyncButton.disabled = false;
         phraseDiv.value = err;
         console.error(err);
 
         recognizer.close();
         recognizer = undefined;
-      });
+      }
+    );
   });
 
   if (!!window.SpeechSDK) {
     SpeechSDK = window.SpeechSDK;
     startRecognizeOnceAsyncButton.disabled = false;
 
-    document.getElementById('content').style.display = 'block';
-    document.getElementById('warning').style.display = 'none';
+    document.getElementById("content").style.display = "block";
+    document.getElementById("warning").style.display = "none";
 
     // in case we have a function for getting an authorization token, call it.
-    if (typeof RequestAuthorizationToken === "function") {
-        RequestAuthorizationToken();
-    }
+    if (typeof RequestAuthorizationToken === "function")
+      RequestAuthorizationToken();
   }
 });
--- index.js
+++ index.js
@@ -12,74 +12,74 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("views"));
-app.use(express.static("assets"));
+app.use('/assets', express.static('assets'));
 
-app.get("/", function (req, res) {
-  res.render("index");
-});
+app.get("/", (_req, res) => {
+    res.render("index");
+  });
 
-app.get("/analyze", function (req, res) {
-  let key, endpoint;
+app.get("/analyze", (req, res) => {
+    let key, endpoint;
 
-  if (typeof process.env.endpoint != "undefined") {
-    key = process.env.key;
-    endpoint = process.env.endpoint;
-  } else {
-    key = require("./creds.js").creds.key;
-    endpoint = require("./creds.js").creds.endpoint;
-  }
-
-  function boldenKeyphrases(text, phrases) {
-    var finalText = text;
-    phrases.forEach((keyword) => {
-      finalText = finalText.replace(
-        keyword,
-        `<span style="font-weight: bold;">${keyword}</span>`
-      );
-    });
-    return finalText;
-  }
-
-  function encodeHTML(text) {
-    text = text.replace(new RegExp(/</gi), "&lt;");
-    text = text.replace(new RegExp(/>/gi), "&gt;");
-    return text;
-  }
-  function GetAnalyzed(txt) {
-    "use strict";
-    const textAnalyticsClient = new TextAnalyticsClient(
-      endpoint,
-      new AzureKeyCredential(key)
-    );
-    async function linkedEntityRecognition(client) {
-      const linkedEntityInput = [encodeHTML(txt)];
-      const entityResults = await client.recognizeLinkedEntities(
-        linkedEntityInput
-      );
-      const keyPhraseResult = await client.extractKeyPhrases(linkedEntityInput);
-      entityResults.forEach((document) => {
-        document.entities.forEach((entity) => {
-          entity.matches.forEach((match) => {
-            return txt.substr(match.offset, match.length + match.offset);
-          });
-        });
-      });
-      res.render("analyze", {
-        returnUrls: entityResults[0].entities,
-        returnText: boldenKeyphrases(
-          encodeHTML(txt),
-          keyPhraseResult[0].keyPhrases
-        ),
-      });
+    if (typeof process.env.endpoint != "undefined") {
+      key = process.env.key;
+      endpoint = process.env.endpoint;
+    } else {
+      key = require("./creds.js").creds.key;
+      endpoint = require("./creds.js").creds.endpoint;
     }
 
-    linkedEntityRecognition(textAnalyticsClient);
-  }
-  GetAnalyzed(
-    req.query.txt ||
+    function boldenKeyphrases(text, phrases) {
+      var finalText = text;
+      phrases.forEach((keyword) => {
+        finalText = finalText.replace(
+          keyword,
+          `<span style="font-weight: bold;">${keyword}</span>`
+        );
+      });
+      return finalText;
+    }
+
+    function encodeHTML(text) {
+      text = text.replace(new RegExp(/</gi), "&lt;");
+      text = text.replace(new RegExp(/>/gi), "&gt;");
+      return text;
+    }
+    function GetAnalyzed(txt) {
+      "use strict";
+      const textAnalyticsClient = new TextAnalyticsClient(
+        endpoint,
+        new AzureKeyCredential(key)
+      );
+      async function linkedEntityRecognition(client) {
+        const linkedEntityInput = [encodeHTML(txt)];
+        const entityResults = await client.recognizeLinkedEntities(
+          linkedEntityInput
+        );
+        const keyPhraseResult = await client.extractKeyPhrases(linkedEntityInput);
+        entityResults.forEach((document) => {
+          document.entities.forEach((entity) => {
+            entity.matches.forEach((match) => {
+              return txt.substr(match.offset, match.length + match.offset);
+            });
+          });
+        });
+        res.render("analyze", {
+          returnUrls: entityResults[0].entities,
+          returnText: boldenKeyphrases(
+            encodeHTML(txt),
+            keyPhraseResult[0].keyPhrases
+          ),
+        });
+      }
+
+      linkedEntityRecognition(textAnalyticsClient);
+    }
+    GetAnalyzed(
+      req.query.txt ||
       "We went to Contoso Steakhouse located at midtown NYC last week for a dinner party, and we adore the spot! They provide marvelous food and they have a great menu. The chief cook happens to be the owner (I think his name is John Doe) and he is super nice, coming out of the kitchen and greeted us all. We enjoyed very much dining in the place! The Sirloin steak I ordered was tender and juicy, and the place was impeccably clean. You can even pre-order from their online menu at www.contososteakhouse.com, call 312-555-0176 or send email to order@contososteakhouse.com! The only complaint I have is the food didn't come fast enough. Overall I highly recommend it!"
-  );
-});
+    );
+  });
 app.listen(port, function () {
   console.log(`Editor listening on port ${port}!`);
 });
